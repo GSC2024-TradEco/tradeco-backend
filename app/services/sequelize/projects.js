42df9c0a272d060aef8f8b02f217b0666cc94417
Johanes Raphael Nandaputra <johanesraphaeln@gmail.com>
2024-02-11 10:14:14 +0700

    Add possible missing materials from user in detail project

--- app/services/sequelize/projects.js
+++ app/services/sequelize/projects.js
@@ -1,5 +1,7 @@
 const { NotFoundError } = require('../../errors');
+const User = require('../../../models').User;
 const Project = require('../../../models').Project;
+const UserWaste = require('../../../models').UserWaste;
 
 const findAllProjects = async (req) => {
   const { page = 1, limit = 10 } = req.query;
@@ -19,7 +21,6 @@ const findAllProjects = async (req) => {
 
 const findOneProject = async (req) => {
   const { id } = req.params;
-
   const project = await Project.findOne({
     where: { id },
     attributes: [
@@ -34,6 +35,41 @@ const findOneProject = async (req) => {
     ],
   });
   if (!project) throw new NotFoundError('Project not found');
+
+  const uid = '1';
+  if (uid) {
+    const user = await User.findOne({
+      where: {
+        uid,
+      },
+    });
+    const userWastes = await UserWaste.findAll({
+      where: {
+        UserId: user.id,
+      },
+      attributes: ['name'],
+    });
+    const userWasteMaterials = userWastes.map((waste) => waste.name);
+
+    const projectMaterials = project.materials;
+    const missingMaterials = projectMaterials.filter(
+      (material) => !userWasteMaterials.includes(material)
+    );
+
+    const usersWithMissingMaterials = await UserWaste.findAll({
+      where: {
+        name: missingMaterials,
+      },
+      attributes: ['name'],
+      include: {
+        model: User,
+        attributes: ['id', 'displayName'],
+      },
+    });
+    project.dataValues.usersWithMissingMaterials = usersWithMissingMaterials;
+
+    return project;
+  }
 
   return project;
 };
